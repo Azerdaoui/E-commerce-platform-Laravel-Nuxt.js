7e30a6c4c9346b72975e311cde94f7bacc578e8c
Azerdaoui Othmane <othmane.azerdaoui@gmail.com>
2020-05-03 02:17:45 +0000

    product variation types

--- cart/database/migrations/2020_05_03_005737_create_product_variation_types_table.php
+++ cart/database/migrations/2020_05_03_005737_create_product_variation_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductVariationsTable extends Migration
+class CreateProductVariationTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateProductVariationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_variations', function (Blueprint $table) {
+        Schema::create('product_variation_types', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
             $table->string('name');
-            $table->integer('price')->nullable();
-            $table->integer('order')->nullable();
             $table->timestamps();
-
-            $table->foreign('product_id')->references('id')->on('products');
         });
     }
 
@@ -32,6 +27,6 @@ class CreateProductVariationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_variations');
+        Schema::dropIfExists('product_variation_types');
     }
 }
